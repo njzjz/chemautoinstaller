5e3fb314ee000af599169239d862ce2797c13d07
曾晋哲 <njzjz@qq.com>
2018-06-16 21:13:45 +0800

    Create anaconda.sh

---
+++ anaconda.sh
@@ -0,0 +1,5 @@
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+bash Anaconda3-5.2.0-Linux-x86_64.sh
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
