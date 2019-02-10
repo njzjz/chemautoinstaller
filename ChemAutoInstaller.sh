aea047cbd36463b770e4c827a49d6bb9ca2242ae
Jinzhe Zeng <njzjz@qq.com>
2019-02-10 09:29:04 +0800

    fix: use anaconda

--- ChemAutoInstaller.sh
+++ ChemAutoInstaller.sh
@@ -1,255 +1,66 @@
-echo ChemAutoInstaller
-echo Author:Jinzhe Zeng
-echo Email:jzzeng@stu.ecnu.edu.cn
+cat <<EOF
+ChemAutoInstaller
+Use Anaconda to install common Chemistry packages.
 
-function init(){
-	if [ ! -n "${CAI_SOFT_DIR}" ];then	
-		CAI_SOFT_DIR=$HOME/ChemAutoInstaller
-	fi
-	CAI_PACKAGE_DIR=${CAI_SOFT_DIR}/packages
-	CAI_BASHRC_FILE=${CAI_SOFT_DIR}/.bashrc
-
-	if [ ! -d "${CAI_SOFT_DIR}" ]; then
-		mkdir "${CAI_SOFT_DIR}"
-	fi
-	if [ ! -d "${CAI_PACKAGE_DIR}" ];then
-		mkdir "${CAI_PACKAGE_DIR}"
-	fi
-	setbashrc
-	checkNetwork
-}
-
-function setbashrc(){
-	if [ ! -f "${CAI_BASHRC_FILE}" ]; then
-		touch ${CAI_BASHRC_FILE}
-		echo 'source '${CAI_BASHRC_FILE}>>$HOME/.bashrc
-	fi
-	source $CAI_BASHRC_FILE
-	source $HOME/.bashrc
-}
-
-function checkNetwork(){
-	ping -c 1 114.114.114.114 
-	if [ $? -eq 0 ];then
-		echo Internet is unblocked.
-	else
-		#ECNU Internet Login
-		echo ChemAutoInstaller needs to connect the Internet.
-		CAI_INTERNET_FILE=${CAI_SOFT_DIR}/.internetlogin
-		if [ -f "${CAI_INTERNET_FILE}" ];then
-			source ${CAI_INTERNET_FILE}
-		fi
-		if [ ! -n "${ECNUUSERNAME}" ];then
-			read -p "Input your ECNU username:" ECNUUSERNAME
-			echo 'ECNUUSERNAME='${ECNUUSERNAME}>>${CAI_INTERNET_FILE}
-		fi
-		if [ ! -n "${ECNUPASSWORD}" ];then
-			read -p "Input your ECNU password:" ECNUPASSWORD
-			echo 'ECNUPASSWORD='${ECNUPASSWORD}>>${CAI_INTERNET_FILE}
-		fi
-		curl -d "action=login&username=${ECNUUSERNAME}&password=${ECNUPASSWORD}&ac_id=1&ajax=1" https://login.ecnu.edu.cn/include/auth_action.php
-	fi
-}
-
-#Anaconda3
-function installAnaconda(){
-	if ! [ -x "$(command -v conda)" ];then
-		echo Installing Anaconda 3...
-		CAI_ANACONDA_DIR=${CAI_SOFT_DIR}/anaconda3
-		wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.2.0-Linux-x86_64.sh -O ${CAI_PACKAGE_DIR}/anaconda3.sh
-		bash ${CAI_PACKAGE_DIR}/anaconda3.sh -b -p ${CAI_ANACONDA_DIR}
-		echo 'export PATH='${CAI_ANACONDA_DIR}'/bin:$PATH'>>${CAI_BASHRC_FILE}
-	    setbashrc
-		setMirror
-		echo Anaconda 3 is installed.
-	fi
-}
-
-function setMirror(){
-	wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py -O ${CAI_PACKAGE_DIR}/oh-my-tuna.py
-	python ${CAI_PACKAGE_DIR}/oh-my-tuna.py
-}
-
-#OpenBabel
-function installOpenBabel(){
-	echo Installing OpenBabel...
-	installAnaconda
-	checkNetwork
-	conda install -y openbabel -c openbabel
-}
-
-#RDkit
-function installRDkit(){
-	echo Installing RDkit...
-	installAnaconda
-	checkNetwork
-	conda install -y rdkit -c rdkit
-}
-
-#ReacNetGenerator
-function installReacNetGenerator(){
-	echo Installing ReacNetGenerator...
-	installAnaconda
-	checkNetwork
-	installOpenBabel
-	installRDkit
-	pip install reacnetgenerator
-}
-
-#LAMMPS
-function installLAMMPS(){
-	echo Installing LAMMPS...
-	CAI_LAMMPS_DIR=${CAI_SOFT_DIR}/lammps
-    wget http://lammps.sandia.gov/tars/lammps-stable.tar.gz -O ${CAI_PACKAGE_DIR}/lammps.tar.gz
-	tar -vxf ${CAI_PACKAGE_DIR}/lammps.tar.gz -C ${CAI_PACKAGE_DIR}
-	mv ${CAI_PACKAGE_DIR}/lammps-* ${CAI_LAMMPS_DIR}	
-	cd ${CAI_LAMMPS_DIR}/src && make yes-user-reaxc && make yes-user-intel
-	if checkIntel ;then
-		cd ${CAI_LAMMPS_DIR}/src && make intel_cpu_intelmpi
-	else
-		installOpenMPI
-		cd ${CAI_LAMMPS_DIR}/src && make mpi
-	fi
-	echo 'export PATH=$PATH:'${CAI_LAMMPS_DIR}/src>>${CAI_BASHRC_FILE}
-	setbashrc
-}
-
-function installOpenMPI(){
-	echo Installing OPENMPI...
-	if ! [ -x "$(command -v mpirun)" ];then
-		CAI_OPENMPI_DIR=${CAI_SOFT_DIR}/openmpi
-		checkNetwork	
-		wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.0.tar.bz2 -O ${CAI_PACKAGE_DIR}/openmpi.tar.bz2
-		tar -vxf ${CAI_PACKAGE_DIR}/openmpi.tar.bz2 -C ${CAI_PACKAGE_DIR}
-		mkdir $CAI_OPENMPI_DIR
-		installgcc
-		installgfortran
-		cd ${CAI_PACKAGE_DIR}/openmpi-3.1.0 && ./configure --prefix=${CAI_OPENMPI_DIR}
-		cd ${CAI_PACKAGE_DIR}/openmpi-3.1.0/ &&	make all install
-        rm -rf ${CAI_PACKAGE_DIR}/openmpi-3.1.0/
-		echo 'export PATH=$PATH:'${CAI_OPENMPI_DIR}/bin>>${CAI_BASHRC_FILE}
-		echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:'${CAI_OPENMPI_DIR}/lib>>${CAI_BASHRC_FILE}
-		setbashrc
-	fi
-}
-
-function checkIntel(){
-	CAI_INTEL_SH=/share/apps/intel/compilers_and_libraries/linux/bin/compilervars.sh
-	if [ -f "${CAI_INTEL_SH}" ];then
-		source ${CAI_INTEL_SH} intel64
-	fi
-	if [ -x "$(command -v mpiicpc)" ];then
-		return 0
-	else
-		return 1
-	fi
-}
-
-function installgcc(){
-	if ! [ -x "$(command -v g++)" ];then
-		installAnaconda
-		conda install -y gcc
-	fi
-}
-
-function installgfortran(){
-	if ! [ -x "$(command -v gfortran)" ];then
-		installAnaconda
-		conda install -y gfortran_linux-64
-	fi
-}
-
-#VMD
-function installVMD(){
-	echo Installing VMD...
-	if ! [ -x "$(command -v vmd)" ];then
-		CAI_VMD_DIR=${CAI_SOFT_DIR}/vmd
-		checkNetwork
-		wget http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.3/files/final/vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz -O ${CAI_PACKAGE_DIR}/vmd.tar.gz
-		tar -vxzf ${CAI_PACKAGE_DIR}/vmd.tar.gz -C ${CAI_PACKAGE_DIR}
-		cat ${CAI_PACKAGE_DIR}/vmd-1.9.3/configure|sed '16c $install_bin_dir="'${CAI_VMD_DIR}'/bin";'|sed '19c $install_library_dir="'${CAI_VMD_DIR}'/lib/$install_name";'>${CAI_PACKAGE_DIR}/vmd-1.9.3/configure_CAI
-		mv ${CAI_PACKAGE_DIR}/vmd-1.9.3/configure_CAI ${CAI_PACKAGE_DIR}/vmd-1.9.3/configure
-		chmod +x ${CAI_PACKAGE_DIR}/vmd-1.9.3/configure
-		cd ${CAI_PACKAGE_DIR}/vmd-1.9.3/ && ./configure LINUXAMD64
-		cd ${CAI_PACKAGE_DIR}/vmd-1.9.3/ && ./configure
-		cd ${CAI_PACKAGE_DIR}/vmd-1.9.3/src && make install
-		rm -rf ${CAI_PACKAGE_DIR}/vmd-1.9.3/
-		echo 'export PATH=$PATH:'${CAI_VMD_DIR}/bin>>${CAI_BASHRC_FILE}
-		setbashrc
-	fi
-}
-
-function installGrace(){
-	checkNetwork
-	wget ftp://plasma-gate.weizmann.ac.il/pub/grace/src/grace-latest.tar.gz -O ${CAI_PACKAGE_DIR}/grace.tar.gz
-	tar -vxzf ${CAI_PACKAGE_DIR}/grace.tar.gz -C ${CAI_PACKAGE_DIR}
-	cd ${CAI_PACKAGE_DIR}/grace-5.1.25 && ./configure --prefix=${CAI_SOFT_DIR}
-	cd ${CAI_PACKAGE_DIR}/grace-5.1.25 && make && make install
-	rm -rf ${CAI_PACKAGES_DIR}/grace-5.1.25
-	echo 'export PATH=$PATH:'${CAI_SOFT_DIR}/grace/bin>>${CAI_BASHRC_FILE}
-	setbashrc
-}
+Author:Jinzhe Zeng
+Email:jzzeng@stu.ecnu.edu.cn
+EOF
 
 function usage(){
-	echo Usage:
-	echo Install softwares one by one:
-	echo bash ChemAutoInstaller.sh --anaconda --openbabel --rdkit --lammps --vmd --openmpi --grace --reacnetgenerator
-	echo Or install all of them:
-	echo bash ChemAutoInstaller.sh --all
+	cat << EOF
+Available command-line options:
+--all, -A                           Install all of softwares
+--anaconda --openbabel --rdkit      Install softwares one by one
+--lammps --vmd --openmpi
+--reacnetgenerator 
+--prefix                            Directory of Anaconda, default is $HOME/anaconda3
+--cn                                If you are in China
+--help, -h                          See help
+EOF
 }
 
-ARGS=$(getopt -a -o Ah -l prefix:,all,anaconda,openbabel,rdkit,lammps,vmd,openmpi,grace,reacnetgenerator,help -- "$@")
+ARGS=$(getopt -a -o Ah -l prefix:,all,anaconda,openbabel,rdkit,lammps,vmd,openmpi,reacnetgenerator,cn,help -- "$@")
 [ $? -ne 0 ] && usage && exit
 [ $# -eq 0 ] && usage && exit
 eval set -- "${ARGS}"
 
+CAI_ANACONDA_DIR=$HOME/anaconda3
+CAI_ANACONDA_URL="https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh"
+CAI_CONDA_FORGE=conda-forge
+
 while true;do
 	case "$1" in
 		--prefix)
-			CAI_SOFT_DIR=$2
+			CAI_ANACONDA_DIR=$2
+			;;
+		--cn)
+			CAI_CN=42
+			CAI_ANACONDA_URL="https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda2-2018.12-Linux-x86_64.sh"
+			CAI_CONDA_FORGE="https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/"
 			;;
 		--anaconda)
-			CAI_IF_INSTALL=42
-			CAI_IF_ANACONDA=42
+			CAI_ANACONDA=42
 			;;
 		--openbabel)
-			CAI_IF_INSTALL=42
-			CAI_IF_OPENBABEL=42
+			CAI_PACKAGE="$CAI_PACKAGE openbabel"
 			;;
 		--rdkit)
-			CAI_IF_INSTALL=42
-			CAI_IF_RDKIT=42
+			CAI_PACKAGE="$CAI_PACKAGE rdkit"
 			;;
 		--lammps)
-			CAI_IF_INSTALL=42
-			CAI_IF_LAMMPS=42
+			CAI_PACKAGE="$CAI_PACKAGE lammps"
 			;;
 		--vmd)
-			CAI_IF_INSTALL=42
-			CAI_IF_VMD=42
+			CAI_PACKAGE="$CAI_PACKAGE vmd"
 			;;
 		--openmpi)
-			CAI_IF_INSTALL=42
-			CAI_IF_OPENMPI=42
-			;;
-		--grace)
-			CAI_IF_INSTALL=42
-			CAI_IF_GRACE=42
+			CAI_PACKAGE="$CAI_PACKAGE openmpi"
 			;;
 		--reacnetgenerator)
-			CAI_IF_INSTALL=42
-			CAI_IF_REACNETGENERATOR=42
+			CAI_PACKAGE="$CAI_PACKAGE reacnetgenerator"
 			;;
 		-A|--all)
-			CAI_IF_INSTALL=42
-			CAI_IF_ANACONDA=42
-			CAI_IF_OPENBABEL=42
-			CAI_IF_RDKIT=42
-			CAI_IF_LAMMPS=42
-			CAI_IF_VMD=42
-			CAI_IF_OPENMPI=42
-			CAI_IF_REACNETGENERATOR=42
-			CAI_IF_GRACE=42
+			CAI_PACKAGE="openbabel rdkit lammps vmd openmpi reacnetgenerator"
 			;;
 		-h|--help)
 			usage
@@ -262,32 +73,14 @@ while true;do
 	shift
 done
 
-if [ ! -z "$CAI_IF_INSTALL" ];then
-	init
-else
-	usage && exit
+test $CAI_ANACONDA || test $CAI_ANACONDA || exit
+
+# check whether conda is installed
+if ! [ -x "$(command -v conda)" ];then
+	echo Installing Anaconda 3...
+	wget -O - $CAI_ANACONDA_URL --progress=dot:giga | bash -b -p $CAI_ANACONDA_DIR
+	export PATH=$CAI_ANACONDA_DIR/bin:$PATH && conda init
 fi
-if [ ! -z "$CAI_IF_ANACONDA" ];then
-	installAnaconda
-fi
-if [ ! -z "$CAI_IF_OPENBABEL" ];then
-	installOpenBabel
-fi
-if [ ! -z "$CAI_IF_RDKIT" ];then
-	installRDkit
-fi
-if [ ! -z "$CAI_IF_LAMMPS" ];then
-	installLAMMPS
-fi
-if [ ! -z "$CAI_IF_VMD" ];then
-	installVMD
-fi
-if [ ! -z "$CAI_IF_OPENMPI" ];then
-	installOpenMPI
-fi
-if [ ! -z "$CAI_IF_REACNETGENERATOR" ];then
-	installReacNetGenerator
-fi
-if [ ! -z "$CAI_IF_GRACE" ];then
-	installGrace
-fi
+# use tsinghua mirror in China
+test $CAI_CN && wget -O - https://tuna.moe/oh-my-tuna/oh-my-tuna.py | python
+test $CAI_PACKAGE && conda install $CAI_PACKAGE -c $CAI_CONDA_FORGE -c openbabel -c njzjz
