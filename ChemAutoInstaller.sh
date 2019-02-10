499d99ba17993841600a5adce710f13e38320047
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-02-10 20:41:26 +0800

    fix: wget

--- ChemAutoInstaller.sh
+++ ChemAutoInstaller.sh
@@ -78,10 +78,7 @@ test "$CAI_ANACONDA" || test "$CAI_PACKAGE" || exit
 # check whether conda is installed
 if ! [ -x "$(command -v conda)" ];then
 	echo Installing Anaconda 3...
-	CAI_ANACONDA_BASH=$(mktemp)
-	wget -O "$CAI_ANACONDA_BASH" "$CAI_ANACONDA_URL" --progress=dot:giga
-	bash "$CAI_ANACONDA_BASH" -b -p "$CAI_ANACONDA_DIR"
-	rm -rf "$CAI_ANACONDA_BASH"
+	wget -O - "$CAI_ANACONDA_URL" --progress=dot:giga | bash -s -- -b -p "$CAI_ANACONDA_DIR"
 	export PATH=$CAI_ANACONDA_DIR/bin:$PATH
 	conda init
 fi
