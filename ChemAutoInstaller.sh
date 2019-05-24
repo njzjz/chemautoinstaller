df68c8a05b801c397bf90da1b20fbd9bb22f545d
Jinzhe Zeng <njzjz@qq.com>
2019-05-24 19:09:52 +0800

    Update ChemAutoInstaller.sh

--- ChemAutoInstaller.sh
+++ ChemAutoInstaller.sh
@@ -71,7 +71,7 @@ test "$CAI_ANACONDA" || test "$CAI_PACKAGE" || exit
 if ! [ -x "$(command -v conda)" ]; then
 	echo Installing Anaconda 3...
 	CAI_ANACONDA_BASH=$(mktemp)
-	wget -# -o "$CAI_ANACONDA_BASH" "$CAI_ANACONDA_URL"
+	curl -# -o "$CAI_ANACONDA_BASH" "$CAI_ANACONDA_URL"
 	bash "$CAI_ANACONDA_BASH" -b -p "$CAI_ANACONDA_DIR"
 	rm -rf "$CAI_ANACONDA_BASH"
 	export PATH=$CAI_ANACONDA_DIR/bin:$PATH
